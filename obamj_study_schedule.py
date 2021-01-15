0368a272e501bdf78d0e128c4b6208930945f74d
Nobuyuki Takahiro <studio.quad9@gmail.com>
2021-01-15 17:14:26 +0900

    参加資格以外のdfは整理する

--- obamj_study_schedule.py
+++ obamj_study_schedule.py
@@ -40,7 +40,7 @@ for label in df:
     if re.search(r"専門|分野別|完全事前申込制", label):
         continue
     else:
-        df[label] = df[label].fillna("〓〓〓〓〓〓〓")
+        df[label] = df[label].fillna("〓〓〓〓")
 
 
 ########## セル内の文字列を正規表現で整形
@@ -62,67 +62,78 @@ for label in df.columns:
 
 ########## 列の整理
 
-########## df["区分・専門"]の生成
-##### df["区分"], df["専門"]の整理
+########## df["区分・専門・分野"]の生成
+##### df["区分"], df["専門"], df["分野別"]の整理
 # 『np.nan』が無効になる場合、『isinstance』が有効！！！！！！
 # というより、『isinstance』一択ではないか？？？？
 df["区分"] = "【" + df["区分"] + "】"
 tmp_div = []
-for div, sp in zip(df["区分"], df["専門"]):
-    if isinstance(sp, float):
+for div, exp, field in zip(df["区分"], df["専門"], df["分野別"]):
+    if isinstance(exp, float) and isinstance(field, float):
         tmp_div.append(div)
     else:
-        tmp_div.append(f"{div}{sp}")
-df["区分・専門"] = tmp_div
+        if isinstance(field, float):
+            tmp_div.append(f"{div}{exp}")
+        else:
+            tmp_div.append(f"{div}{exp}〓{field}")
+df["区分・専門・分野"] = tmp_div
 
 
-########## df["研修内容"]の生成
-##### df["分野別"]の整理
-tmp_field = []
-for field in df["分野別"]:
-    if isinstance(field, float):
-        tmp_field.append(field)
+########## df["実施日"]の変換
+tmp_date = []
+for date in df["実施日"]:
+    date_arr = ntzstr.extdate(date, "w")
+    tmp_date.append(f"{date_arr[0]}/{date_arr[1]}{date_arr[2]}")
+df["実施日"] = tmp_date
+
+
+########## df["実施日"]の変換
+tmp_time = []
+for time in df["実施時間"]:
+    # アスキーの『：』が入っている場合。
+    tmp_time.append(time.replace(':', '：'))
+df["実施時間"] = tmp_time
+
+
+########## df["日時"]の生成
+df["日時"] = df["区分・専門・分野"] + "▼" + df["実施日"] + "▼" + df["実施時間"]
+
+
+tmp_teacher = []
+for cell in df["講師"]:
+    # 行内改行がある場合は『〓』を入れて知らせる。
+    cell = re.sub("▽", "〓", cell)
+    tmp_teacher.append(cell)
+df["講師"] = tmp_teacher
+
+
+########## df["内容・講師"]の生成
+tmp_contents = []
+for title, teacher in zip(df["研修等の名称"], df["講師"]):
+    tmp_contents.append(f"{title}▼◇{teacher}")
+df["内容・講師"] = tmp_contents
+
+
+
+########## df["完全事前申込制"]の整理
+tmp_app = []
+for entry in df["完全事前申込制"]:
+    if isinstance(entry, float):
+        tmp_app.append(entry)
     else:
-        tmp_field.append(f'分野別登録弁護士制度指定研修（{field}）')
-df["分野別"] = tmp_field
+        entries = "、".join(re.findall(r"（(.+?)）", entry))
+        tmp_app.append(f"{entries}、完全事前申込制")
+df["完全事前申込制"] = tmp_app
 
 
-# df["講師"] = df["講師"].fillna("〓〓〓〓〓〓〓")
-# print(df["講師"][0])
-# # df["内容"] = df["分野別"] + "▼" + df["研修等の名称"] + "▼◇" + df["講師"]
-# # print(df["区分・専門"])
-
-# # df[""]
-
-
-# tmp_date = []
-# for cell in df["実施日"]:
-#     cell = re.sub("(\d\d?)月(\d\d?)日（([月火水木金土日])）", r"\1/\2\3", cell)
-#     tmp_date.append(cell)
-# df["実施日"] = tmp_date
-# # 列の入替
-# df["日時"] = df["区分"] + df["専門"] + "▼" + df["実施日"] + "▼" + df["実施時間"]
-# df["単位"] = df["継続\n研修\n単位数"]
-# tmp_teacher = []
-# for cell in df["講師"]:
-#     cell = re.sub("▽", "、", cell)
-#     tmp_teacher.append(cell)
-# df["講師"] = tmp_teacher
-
-# df["備考"] = "◆" + df["主催者"] + "▼□" + df["参加資格等"] + "／" + df["参加費"] + "▼■" + df["実施場所"] + "▼" + df["一時保育"]
-# # 変更した内容を元のファイルへ上書き保存する。
-# df.to_csv(file,
-#     encoding = "utf-8",
-#     index = False,
-#     columns = ["日時", "単位", "内容", "備考"],
-#     sep = "\t")
-
-
-# for cell in df["実施日"]:
-#     cell = re.sub("(\d\d?)月(\d\d?)日（([月火水木金土日])）", r"\1/\2\3", cell)
-#     tmp_date.append(cell)
-
-
+########## df["資格"]の生成
+tmp_require = []
+for require, admission, application in zip(df["参加資格等"], df["参加費"], df["完全事前申込制"]):
+    if isinstance(application, float):
+        tmp_require.append(f"{require}／{admission}")
+    else:
+        tmp_require.append(f"{require}（{application}）／{admission}")
+df["資格"] = tmp_require
 
 
 ##### 出力
@@ -130,5 +141,5 @@ path_to_gen_file = os.path.join("./_gen", f"gene_{filename}")
 df.to_csv(path_to_gen_file,
     encoding = "utf-8",
     index = False,
-    columns = ["実施日", "実施時間", "単位数", "実施形態", "区分", "専門", "分野別", "研修等の名称", "講師", "主催者", "実施場所", "参加費", "参加資格等", "一時保育", "完全事前申込制"],
+    columns = ["日時", "単位数", "内容・講師", "資格"],
     sep = "\t")
